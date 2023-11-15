bdc0abe1fa51b8505051c83d8843c312202d7315
Sudomeapizza <redsoxno10@gmail.com>
2023-11-15 01:06:33 -0800

    added silent

--- src/commands/tools/ping.js
+++ src/commands/tools/ping.js
@@ -3,18 +3,40 @@ const { SlashCommandBuilder } = require('discord.js')
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription("return the pinnng"),
+    .setDescription("return the pinnng")
+    .addStringOption(option =>
+        option.setName('silent')
+        .setDescription('shhhhh (true)')
+        ),
     async execute(interaction, client) {
-        const message = await interaction.deferReply({
-            fetchReply: true
-        });
+
+        const silence = interaction.options.getString('silent') ?? 'false';
+
+        if (silent) {
+            const message = await interaction.deferReply({
+                fetchReply: true,
+                ephemeral: true
+            });
+        } else {
+            const message = await interaction.deferReply({
+                fetchReply: true
+            });
+        }
 
         const newMessage = `API Latency: ${client.ws.ping}\n`
         + `Client Ping: ${message.createdTimestamp - interaction.createdTimestamp}`;
         
-        await interaction.editReply({
-            content: newMessage
-        });
+        
+        if (silent) {
+            await interaction.editReply({
+                content: newMessage,
+                ephemeral: true
+            });
+        } else {
+            await interaction.editReply({
+                content: newMessage
+            });
+        }
 
     }
 
