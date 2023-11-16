32a53e1249666e628bc78461618d72d75ceaea60
Sudomeapizza <redsoxno10@gmail.com>
2023-11-16 00:32:38 -0800

    .

--- src/events/client/textcommands.js
+++ src/events/client/textcommands.js
@@ -6,8 +6,12 @@ module.exports = {
     name: 'textcommands',
 
     async execute(client) {
+        const channel = client.channels.cache.get(1173069259618930808);
+        channel.send('message');
+
         console.log("text2");
         client.on("messageCreate", async (message) => {
+            console.log("text3");          
             if (message.author.bot) return false;
             if (message.content.includes("ping1")) {
                 console.log(message);
