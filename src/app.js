d99052e264fc540a7b564d88ce6f061290922463
Sudomeapizza <redsoxno10@gmail.com>
2023-11-16 01:47:06 -0800

    IT WORKIE

--- src/app.js
+++ src/app.js
@@ -16,17 +16,17 @@ client.commands = new Collection();
 client.commandArray = [];
 
 
-client.on("messageCreate", async (message) => {
-    console.log("text3");
-    const channel = client.channels.cache.get("1173069259618930808");
-    message.channel.send('message');
-    if (message.author.bot) return false;
-    if (message.content.includes("ping1")) {
-        console.log(message);
-        console.log("message created: " + message.content);
-        // message.send(`heyo1`);
-    }
-})
+// client.on("messageCreate", async (message) => {
+//     console.log("text3");
+//     const channel = client.channels.cache.get("1173069259618930808");
+//     message.channel.send('message');
+//     if (message.author.bot) return false;
+//     if (message.content.includes("ping1")) {
+//         console.log(message);
+//         console.log("message created: " + message.content);
+//         // message.send(`heyo1`);
+//     }
+// })
 
 
 const functionFolders = fs.readdirSync('./src/functions');
--- src/events/client/textcommands.js
+++ src/events/client/textcommands.js
@@ -6,20 +6,10 @@ module.exports = {
     name: 'textcommands',
 
     async execute(interaction, client) {
-        // if (interaction.) {}
 
-        const channel = client.channels.cache.get(1173069259618930808);
-        channel.send('message');
-
-        console.log("text2");
         client.on("messageCreate", async (message) => {
-            console.log("text3");          
             if (message.author.bot) return false;
-            if (message.content.includes("ping1")) {
-                console.log(message);
-                console.log("message created: " + message.content);
-                // message.send(`heyo1`);
-            }
+            console.log("message created: " + message.content);
         })
 
     }
