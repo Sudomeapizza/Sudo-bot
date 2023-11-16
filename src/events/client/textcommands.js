11336a6871f51a859688126fd44e4a65c447ce80
Sudomeapizza <redsoxno10@gmail.com>
2023-11-15 23:54:38 -0800

    .

--- src/events/client/textcommands.js
+++ src/events/client/textcommands.js
@@ -9,17 +9,18 @@ module.exports = {
             if (message.author.bot) return false;
             if (message.content.includes("ping1")) {
                 console.log(message);
+                console.log("message created: " + message.content);
                 // message.send(`heyo1`);
             }
         })
         
-        client.on("messageCreate", async (message) => {
-            if (message.author.bot) return false;
-            if (message.content.includes("ping1")) {
-                console.log(message);
-                // message.send(`heyo1`);
-            }
-        })
+        // client.on("messageCreate", async (message) => {
+        //     if (message.author.bot) return false;
+        //     if (message.content.includes("ping1")) {
+        //         console.log(message);
+        //         // message.send(`heyo1`);
+        //     }
+        // })
 
     }
 
