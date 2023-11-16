27f3c6b3ba1300eb623b6b91c28db7392fc0c49f
Sudomeapizza <redsoxno10@gmail.com>
2023-11-15 23:36:52 -0800

    .

--- src/commands/tools/textcommands.js
+++ src/commands/tools/textcommands.js
@@ -1,18 +1,18 @@
 const { SlashCommandBuilder } = require('discord.js')
 
-client.on("messageCreate", async (message) => {
-    if (message.author.bot) return false;
-    if (message.content.includes("ping1")) {
-        console.log(message);
-        // message.send(`heyo1`);
-    }
-})
+
 
 module.exports = {
     async execute(interaction, client) {
 
-        const silence = interaction.options.getBoolean('silents') || false;
-        
+        // const silence = interaction.options.getBoolean('silents') || false;
+        client.on("messageCreate", async (message) => {
+            if (message.author.bot) return false;
+            if (message.content.includes("ping1")) {
+                console.log(message);
+                // message.send(`heyo1`);
+            }
+        })
 
     }
 
