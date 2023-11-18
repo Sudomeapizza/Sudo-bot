4594f499942bafbb4761b8c6adefed4b3549a5f6
Sudomeapizza <redsoxno10@gmail.com>
2023-11-18 05:34:03 -0800

    await?

--- src/functions/handlers/textcommands.js
+++ src/functions/handlers/textcommands.js
@@ -36,7 +36,7 @@ module.exports = (client) => {
                 if (timestamp) {
                     message.channel.send(userMessage.replace(`${info[1]} at ${info[0]}`,`${timestamp[0]}${timestamp[1]}`));
                 } else {
-                    interaction.reply({
+                    await interaction.reply({
                         content: "You do not have a region set internally, please specify your region.",
                         ephemeral: true
                     });
