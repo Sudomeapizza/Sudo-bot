8a904b820ebaddc7235d0197225986bafa655813
Sudomeapizza <redsoxno10@gmail.com>
2023-11-17 21:31:43 -0800

    .

--- src/commands/tools/say.js
+++ src/commands/tools/say.js
@@ -13,12 +13,12 @@ module.exports = {
         const usermessage = interaction.options.getString('message');
         // var message;
 
-        client.channels.cache.get(`${interaction.channelId}`).send(usermessage);
+        // client.channels.cache.get(`${interaction.channelId}`).send(usermessage);
 
         await interaction.reply({
             // message: ""
         });
-        await interaction.deleteReply({});
+        // await interaction.deleteReply({});
 
 
 
