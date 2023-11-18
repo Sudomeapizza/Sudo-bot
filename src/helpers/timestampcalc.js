96d606d861242aedf8e1b0b67e0b1019bf5d693a
Sudomeapizza <redsoxno10@gmail.com>
2023-11-17 23:21:37 -0800

    .

--- src/helpers/timestampcalc.js
+++ src/helpers/timestampcalc.js
@@ -1,4 +1,4 @@
-function timeStampCalc(date, time, region, format){
+export function timeStampCalc(date, time, region, format){
     // this is in UTC
     var timestamp = Date.parse(`${date} ${time}`)/1000;
             
@@ -29,5 +29,3 @@ function timeStampCalc(date, time, region, format){
 
     return fullResponse;
 }
-
-export {timeStampCalc};
