042b8960bbb8df1f721ab4e760fb94e14c34b163
Sudomeapizza <redsoxno10@gmail.com>
2023-11-18 05:24:02 -0800

    maybe silent now?

--- src/functions/handlers/textcommands.js
+++ src/functions/handlers/textcommands.js
@@ -33,7 +33,7 @@ module.exports = (client) => {
                 if (timestamp) {
                     message.channel.send(userMessage.replace(`${info[1]} at ${info[0]}`,`${timestamp[0]}${timestamp[1]}`));
                 } else {
-                    message.channel.send({content:"You do not have a region set internally, please specify your region.", ephemeral:true});
+                    message.reply({content:"You do not have a region set internally, please specify your region.", ephemeral:true});
                 }
 
             } else {
