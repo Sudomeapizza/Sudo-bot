e014aff72afdbf012714dd5ef195eb5a98918e71
Sudomeapizza <redsoxno10@gmail.com>
2023-11-19 03:53:27 -0800

    ?..?

--- src/helpers/website.js
+++ src/helpers/website.js
@@ -7,7 +7,7 @@ function getLink() {
         'https://api.example.com/data'];
 
         console.log("________" + shell.exec('echo $(wget -q -O "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&rvprop=content&grnlimit=1")'));
-        console.log("_-_-_-_-" + shell.exec('echo $(wget -q -O "https://example.com")'));
+        console.log("_-_-_-_-" + shell.exec('echo ./src/helpers/wget.sh")'));
         
     // async function downloadFile(url) {
     //     try {
--- src/helpers/wget.sh
+++ src/helpers/wget.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-url="$1"
+url="https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&rvprop=content&grnlimit=1"
 
 # Use wget to retrieve data and capture the output
 data=$(wget -q -O "$url")
