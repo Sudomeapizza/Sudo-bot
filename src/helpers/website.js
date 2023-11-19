1a355820d75ce8a5c9cf311df80372147fc8112e
Sudomeapizza <redsoxno10@gmail.com>
2023-11-19 05:45:32 -0800

    .

--- src/helpers/website.js
+++ src/helpers/website.js
@@ -18,7 +18,7 @@ class website {
     // this just prints to the console for no reason...
     // try without echo?
     // var readableContent = shell.exec(`echo $(./src/helpers/wget.sh "${source[0]}")`);
-    var readableContent = shell.exec(`$(./src/helpers/wget.sh "${source[0]}")`);
+    var readableContent = `{"warnings":{"main":{"*":"Unrecognized parameter: rvprop."}},"batchcomplete":"","continue":{"grncontinue":"0.513767514422|0.513767685666|8605127|0","continue":"grncontinue||"},"query":{"pages":{"69211489":{"pageid":69211489,"ns":0,"title":"Meanings of minor planet names: 605001\u2013606000"}}}}`;
     const jsonData = JSON.parse(readableContent);
     const pagesValue = jsonData.query.pages;
     const firstPageKey = Object.keys(pagesValue)[0];
