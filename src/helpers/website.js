3a40cc079b4f9e4c2c66bd6df683ca346db32d27
Sudomeapizza <redsoxno10@gmail.com>
2023-11-19 05:53:40 -0800

    lkasd

--- src/helpers/website.js
+++ src/helpers/website.js
@@ -19,7 +19,7 @@ class website {
     // this just prints to the console for no reason...
     // try without echo?
     console.log(0);
-    var readableContent = shell.exec(`echo $(./src/helpers/wget.sh "${source[0]}", )`, {silent:true});
+    var readableContent = shell.exec(`echo $(./src/helpers/wget.sh "${source[0]}", )`);
     // var readableContent = shell.exec(`$(./src/helpers/wget.sh "${source[0]}")`);
     console.log(readableContent);
     console.log(1);
