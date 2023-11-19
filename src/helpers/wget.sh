c5c83af987f39af9bae80e0c8a3b24e0ca329b94
Sudomeapizza <redsoxno10@gmail.com>
2023-11-19 03:56:13 -0800

    asf

--- src/helpers/wget.sh
+++ src/helpers/wget.sh
@@ -3,7 +3,7 @@
 url="https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&rvprop=content&grnlimit=1"
 
 # Use wget to retrieve data and capture the output
-data=$(wget -q -O "$url")
+data=$(wget -q -O $url) 
 
 # Display the retrieved data
 echo "$data"
