129f84f2532e5c4dd9713c075f4ed0701fe7292c
Sudomeapizza <redsoxno10@gmail.com>
2023-11-19 01:36:52 -0800

    take7

--- src/helpers/replycalc.js
+++ src/helpers/replycalc.js
@@ -41,79 +41,34 @@ function random(array){
 }
 
 function randomWiki(){
-
-
-
     const source = ['https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&rvprop=content&grnlimit=1',
     'https://google.com',
     'https://api.example.com/data'];
-    var outputstuff;
-
- 
-    // request.open("GET", source[0]);
-    
-    // XMLHttpRequest.onreadystatechange = function() {
-    //     if (this.readyState == 4 && this.status == 200) {
-    //     // Typical action to be performed when the document is ready:
-    //     outputstuff = XMLHttpRequest.responseText;
-    //     }
-    // };
-    // XMLHttpRequest.open("GET", source[0], true);
-    // XMLHttpRequest.send();
-
     const axios = require('axios');
 
-async function downloadFile(url) {
-  try {
-    // Make a GET request to the URL to download the file
-    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    async function downloadFile(url) {
+    try {
+        // Make a GET request to the URL to download the file
+        const response = await axios.get(url, { responseType: 'arraybuffer' });
 
-    // Get the file content as a Buffer
-    const fileContent = Buffer.from(response.data, 'binary');
-    const readableContent = fileContent.toString('utf-8');
-    const jsonData = JSON.parse(readableContent);
-    const pagesValue = jsonData.query.pages;
-    const firstPageKey = Object.keys(pagesValue)[0];
-    const pageNumber = pagesValue[firstPageKey].pageid;
+        // Get the file content as a Buffer
+        const fileContent = Buffer.from(response.data, 'binary');
+        const readableContent = fileContent.toString('utf-8');
+        const jsonData = JSON.parse(readableContent);
+        const pagesValue = jsonData.query.pages;
+        const firstPageKey = Object.keys(pagesValue)[0];
+        const pageNumber = pagesValue[firstPageKey].pageid;
 
-    // You can now use the fileContent variable as needed
-    console.log('https://en.wikipedia.org/?curid=', pageNumber.replace(/\s+/g, ''));
-  } catch (error) {
-    console.error('Error downloading file:', error.message);
-  }
-}
+        // You can now use the fileContent variable as needed
+        console.log(('https://en.wikipedia.org/?curid=' + pageNumber).replace(/\s+/g, ''));
+    } catch (error) {
+        console.error('Error downloading file:', error.message);
+    }
+    }
 
-// Replace 'YOUR_FILE_URL' with the actual URL of the file you want to download
-const fileUrl = source[0];
-downloadFile(fileUrl);
-
-    // function handler() {
-    //     if(this.status == 200 &&
-    //       this.responseXML != null &&
-    //       this.responseXML.getElementById('test').textContent) {
-    //       // success!
-    //       console.log(this.responseXML.getElementById('test').textContent);
-    //     }
-    //   }
-      
-    //   var client = new XMLHttpRequest();
-    //   client.onload = handler;
-    //   client.open("GET", "unicorn.xml");
-    //   client.send();
-
-    // var info = wget.download(source[1], "");
-    // console.log(`${JSON.parse(JSON.stringify(outputstuff))}`)
-    // console.log(`https://en.wikipedia.org/?curid=${JSON.parse(info).query.pages}`);
-
-
-
-    // article_page = requests.get(wiki_url)
-
-    // soup = BeautifulSoup(article_page.text, "html.parser")
-    // article_title = soup.find(id='firstHeading')
-    
-    // response = `${article_title.string}`
-    // return response;
+    // Replace 'YOUR_FILE_URL' with the actual URL of the file you want to download
+    const fileUrl = source[0];
+    downloadFile(fileUrl);
 }
 
 module.exports = { getArray }
