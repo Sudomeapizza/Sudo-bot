55ee22688aeb439bbd4c954332c7e1c8d50b91c9
Sudomeapizza <redsoxno10@gmail.com>
2023-11-19 17:21:57 -0800

    oianfs

--- src/helpers/website.js
+++ src/helpers/website.js
@@ -2,30 +2,26 @@
 // const { ReactionCollector } = require('discord.js');
 const shell = require('shelljs');
 
-var _link;
-
-class website {
+class Website {
   constructor() {
     console.log("Workie13");
-    this.getLink();
+    this.fetchLink();
   }
 
-  get getLink() {
+  fetchLink() {
     const source = [
       'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&rvprop=content&grnlimit=1',
       'https://google.com',
       'https://api.example.com/data'
     ];
 
-    // fixed the console readout, but now it sends a broken message...
-    var readableContent = shell.exec(`echo $(./src/helpers/wget.sh "${source[0]}")`, {silent:true});
+    var readableContent = shell.exec(`echo $(./src/helpers/wget.sh "${source[0]}")`, { silent: true });
     const jsonData = JSON.parse(readableContent);
     const pagesValue = jsonData.query.pages;
     const firstPageKey = Object.keys(pagesValue)[0];
     const pageNumber = pagesValue[firstPageKey].pageid;
 
     this._link = ('https://en.wikipedia.org/?curid=' + pageNumber).replace(/\s+/g, '');
-
   }
 
   static get theLink() {
@@ -34,4 +30,4 @@ class website {
   }
 }
 
-module.exports = { website };
+module.exports = { Website };
