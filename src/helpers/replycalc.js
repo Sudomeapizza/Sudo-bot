b54cca3a5ca7523eb082f1696e2220fd760b52c4
Sudomeapizza <redsoxno10@gmail.com>
2023-11-19 21:45:11 -0800

    kjbasfj

--- src/helpers/replycalc.js
+++ src/helpers/replycalc.js
@@ -1,4 +1,6 @@
 const { website, theLink } = require('./website.js');
+const shell = require('shelljs');
+
 
 var bloodyGifs = ["https://cdn.discordapp.com/attachments/669372366710898688/1175332478563651664/image0.gif?ex=656ad8ab&is=655863ab&hm=dc4afa1be5b9f0f72829d88f9b9944a36c1f97abb8760138712009ba264b9b1a&",
 "https://cdn.discordapp.com/attachments/669372366710898688/1175332479251521585/image1.gif?ex=656ad8ab&is=655863ab&hm=7cea1d7a01a7b5bdbfaa75c3202d0fbe6df0cc59a6ffb658769eba49736114ec&",
