bafd95c0a7db7896a6aac7b9201bdd304790749c
Sudomeapizza <redsoxno10@gmail.com>
2023-11-27 02:00:16 -0800

    updated regex

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -7,3 +7,6 @@ module.exports = {
         client.channels.cache.get(`1178352991150014505`).send("Booted and ready!");
     }
 }
+
+// /\b(?:mon|tue|wed|thurs|fri|sat|sun|monday|tuesday|wednesday|thursday|friday|saturday|sunday|today|tonight)\b\s(?:at|@)\s((?:[0]\d|[01]\d|2[0-3]):?[0-5]\d)?/
+// /\b(?:mon|tue|wed|thurs|fri|sat|sun|monday|tuesday|wednesday|thursday|friday|saturday|sunday|today|tonight)\b\s(?:at|@)\s((?:[0]\d|[01]?\d|2[0-3]):?[0-5]\d)?/
--- src/helpers/regex.js
+++ src/helpers/regex.js
@@ -1,9 +1,6 @@
 
 function dateRegex(sentence) {
-    // const regexPattern = /\b(?:mon|tue|wed|thurs|fri|sat|sun|monday|tuesday|wednesday|thursday|friday|saturday|sunday|today|tonight)\b\s(?:at|@)\s((:0?[0-9]|1[0-9]|2[0-4])(?::[0-5][0-9])?)\s?(am|pm)?/i;
-    const regexPattern = /\b(?:mon|tue|wed|thu|thurs|fri|sat|sun|monday|tuesday|wednesday|thursday|friday|saturday|sunday|today|tonight)\b\s(?:at|@)\s((?:[01]\d|2[0-3]):?[0-5]\d)?/i;
-    // const regexPattern = /\b(?:mon|tue|wed|thurs|fri|sat|sun|today|tonight)\b\s(?:at|@)\s((:0?[0-9]|1[0-9]|2[0-4])(?::[0-5][0-9])?)\s?(am|pm)?/i;
-
+    const regexPattern = /\b(?:mon|tue|wed|thu|thurs|fri|sat|sun|monday|tuesday|wednesday|thursday|friday|saturday|sunday|today|tonight)\b\s(?:at|@)\s((?:[0]\d|[01]?\d|2[0-3]):?[0-5]\d)?/i;
     const match = sentence.match(regexPattern);
     var data = [];
 
