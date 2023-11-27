a526e1853f6de4fa917b29b53dbaa8d451de9023
Sudomeapizza <redsoxno10@gmail.com>
2023-11-27 01:48:50 -0800

    update msg when bot is ready

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -4,5 +4,6 @@ module.exports = {
     async execute(client) {
         console.log(`${client.user.tag} is online`)
         console.log(`yo`);
+        client.channels.cache.get(`1178352991150014505`).send("Booted and ready!");
     }
 }
