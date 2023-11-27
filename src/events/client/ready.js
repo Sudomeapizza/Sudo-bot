0de80ac7dc22319d14b6274bae628f5e60f8398d
Sudomeapizza <redsoxno10@gmail.com>
2023-11-27 03:10:58 -0800

    gimme dat back

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
-        console.log(`${client.user.tag} is online`)
+        console.log(`${client.user.tag} is online`);
+        client.channels.cache.get(`1178352991150014505`).send("Booted and ready!");
     }
 }
