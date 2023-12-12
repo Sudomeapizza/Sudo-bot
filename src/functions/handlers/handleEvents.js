4a0609ce3c9c3a2f98c0870f16f903f508bba6be
Sudomeapizza <redsoxno10@gmail.com>
2023-12-11 23:04:07 -0800

    ojans?

--- src/functions/handlers/handleEvents.js
+++ src/functions/handlers/handleEvents.js
@@ -9,6 +9,7 @@ module.exports = (client) => {
                 case "client":
                     for (const file of eventFiles) {
                         const event = require(`../../events/${folder}/${file}`);
+                        console.log("logging " + file);
                         if (event.once) client.once(event.name, (...args) => event.execute(...args,client));
                         else client.on(event.name, (...args) => event.execute(...args, client));
                     }
