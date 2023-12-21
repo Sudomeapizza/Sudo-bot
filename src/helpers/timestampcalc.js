d5b2e629584bca4d40f615f43bab7a16001b3910
Sudomeapizza <redsoxno10@gmail.com>
2023-12-20 20:15:48 -0800

    duh?

--- src/helpers/timestampcalc.js
+++ src/helpers/timestampcalc.js
@@ -39,7 +39,7 @@ function timeStampCalc(date, time, region, format, internal = false, userId, cli
     } else {
         timestamp = new Date(new Date()
             .toLocaleString("en-US", {
-                timeZone: client.getTimeZone("210932800000491520")
+                timeZone: localTimeZone
             })).setHours(time1, time2, 0, 0);
         if (internal) {
             fullResponse = [`<t:${timestamp}:F>`,`<t:${timestamp}:R>`];
