c764b23b2c8db2d167391a65bf62f68dd8d4857a
Joe <redsoxno10@gmail.com>
2023-12-23 05:05:12 -0800

    asdads

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -3,6 +3,6 @@ module.exports = {
     once: true,
     async execute(client) {
         console.log(`${client.user.tag} is online`);
-        client.channels.cache.get(`1178644165731557436`).send("Booted and ready!");
+        client.channels.cache.get(`1178352991150014505`).send("Booted and ready!");
     }
 }
