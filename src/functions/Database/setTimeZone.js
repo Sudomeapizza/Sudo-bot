d722ea0167766aca81f39f775a352b2cfc6f6579
Joe <redsoxno10@gmail.com>
2024-01-02 21:08:24 -0800

    wadawd

--- src/functions/Database/setTimeZone.js
+++ src/functions/Database/setTimeZone.js
@@ -3,22 +3,34 @@ const mongoose = require("mongoose");
 
 module.exports = (client) => {
     client.setTimeZone = async (userId, guildId, inputTimeZone) => {
-        var userTimeZone = await timeZone.findOneAndUpdate({
+        // Check if the userTimeZone already exists in the database
+        let userTimeZone = await timeZone.findOne({
             userId: userId,
-            // guildId: guildId,
             timeZone: inputTimeZone,
         });
+
         if (!userTimeZone) {
-            userTimeZone = await new timeZone({
+            console.log("Attempting to set a new time zone entry");
+
+            // Create a new time zone entry
+            userTimeZone = new timeZone({
                 _id: new mongoose.Types.ObjectId(),
                 userId: userId,
-                guildId: guildId,
                 timeZone: inputTimeZone,
             });
+
+            // Save the new entry to the database
             await userTimeZone.save().catch(console.error);
             return userTimeZone;
         } else {
+            console.log("Attempting to update an existing time zone entrys");
+
+            // Update the existing time zone entry with the new information if needed
+            userTimeZone.timeZone = inputTimeZone;
+
+            // Save the updated entry to the database
+            await userTimeZone.save().catch(console.error);
             return userTimeZone;
         }
-    }
-}
+    };
+};
