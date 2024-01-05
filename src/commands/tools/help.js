874c0102ef326574ed96ff9c50cc60c5036dc4aa
Sudomeapizza <redsoxno10@gmail.com>
2024-01-05 12:07:48 -0800

    update help message?

--- src/commands/tools/help.js
+++ src/commands/tools/help.js
@@ -7,20 +7,20 @@ module.exports = {
     async execute(interaction, client) {
         const user = interaction.user;
 
-        const message = `Get started with \`/settimezone\` to globally use your timezone to auto convert your messages\n
-                        Remove your data with \`/removetimezonedata\` at anytime!
+        const message = "Get started with \`/settimezone\` to globally use your timezone to auto convert your messages\n" +
+                        "Remove your data with \`/removetimezonedata\` at anytime!\n" +
 
-                        I can recognize any combination of these commands!
-                        <day> <at/@> <time>
-                        !(Date) <at/@> <time>
+                        "I can recognize any combination of these commands!\n" +
+                        "<day> <at/@> <time>\n" +
+                        "!(Date) <at/@> <time>\n" +
 
-                        Tuesday at 12:00am
-                        today at 15:00
-                        tomorrow at 5:00 pm
-                        Tuesday at 1200
-                        tonight @ 20
-                        !(Dec 14 2025) at 4pm
-                        !(Jan 10) at 15`;
+                        "Tuesday at 12:00am\n" +
+                        "today at 15:00\n" +
+                        "tomorrow at 5:00 pm\n" +
+                        "Tuesday at 1200\n" +
+                        "tonight @ 20\n" +
+                        "!(Dec 14 2025) at 4pm\n" +
+                        "!(Jan 10) at 15`";
 
         const embed = new EmbedBuilder()
             .setTitle(`Help:`)
