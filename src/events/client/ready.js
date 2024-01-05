cee5d696edf025834f33d713caa4e32a1bbd4f94
Joe <redsoxno10@gmail.com>
2024-01-04 19:40:16 -0800

    this fix it?

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -3,6 +3,15 @@ module.exports = {
     once: true,
     async execute(client) {
         console.log(`${client.user.tag} is online`);
-        client.channels.cache.get(`1178644165731557436`).send("Booted and ready!");
+        // client.channels.cache.get(`1178644165731557436`).send("Booted and ready!");
+
+        // Fetch the guild (server) by ID
+        const guild = client.guilds.cache.get("1174218330962395146");
+
+        // Fetch the channel by ID
+        const channel = guild.channels.cache.get(`1178644165731557436`);
+
+        // Send the message "Booted and ready!" to the specified channel
+        channel.send('Booted and ready!');
     }
 }
