9909d7be2c0558e31690065f561eea9a60de63ab
Sudomeapizza <redsoxno10@gmail.com>
2024-01-05 19:33:14 -0800

    aslinf

--- src/helpers/replycalc.js
+++ src/helpers/replycalc.js
@@ -90,9 +90,9 @@ function restart(process) {
 
 function gitpull(app) {
     if (app == 0) {
-        return shell.exec(`cd ~/DiscordBot/; pull`, { silent: true }).toString();
+        return shell.exec(`cd ~/DiscordBot/; pull`, { silent: true }).toString() || "None:10";
     } else if (app == 1) {
-        return shell.exec(`cd ~/DiscordTestBot/; git pull`, { silent: true }).toString();
+        return shell.exec(`cd ~/DiscordTestBot/; git pull`, { silent: true }).toString() || "None:11";
     } else {
         return "wat";
     }
