759d5fe5d4b8a3448e0be441e39c074a1abe67f7
Joe <redsoxno10@gmail.com>
2024-03-02 00:47:44 -0800

    test 2

--- src/helpers/replycalc.js
+++ src/helpers/replycalc.js
@@ -107,7 +107,7 @@ function gitpull(app) {
 }
 
 function pushCode() {
-    return shell.exec(`cd ~/DiscordBot/; gh pr create --fill`, { silent: true }).toString() || "None:12";
+    return shell.exec(`cd ~/DiscordTestBot/; gh pr create --title "Title msg" --body "Body msg"; gh pr merge --admin -- merge`, { silent: true }).toString() || "None:12";
 }
 
 function pokemon(process) {
