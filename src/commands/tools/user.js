5ed379bbc47a72abedb9b2bf0ab64ac042c5fe73
Joe <redsoxno10@gmail.com>
2024-03-14 04:46:24 -0700

    User command Updated Final

--- src/commands/tools/user.js
+++ src/commands/tools/user.js
@@ -19,27 +19,6 @@ module.exports = {
 
         const target = interaction.options.getUser('user');
         const silence = interaction.options.getBoolean('silent') || false;
-        var newMessage = "";
-        
-        // if (!target) {
-        //     newMessage = `Display name: ${interaction.user.displayName}\n`
-        //     + `Username: ${interaction.user.username}\n`
-        //     + `User ID: ${interaction.user.id}`;
-        // } else {
-            newMessage = `Display name: ${target.displayName}\n`
-            + `Username: ${target.username}\n`
-            + `User ID: ${target.id}`;
-        // }
-
-        // https://discordjs.guide/popular-topics/embeds.html#embed-preview
-
-        // console.log(interaction.guild.members.cache.get(target.id));
-        // console.log(interaction.guild.members.cache);
-        // console.log(interaction.member);
-        // console.log(interaction.member.guild);
-        // console.log(interaction.member.joinedAt);
-        // console.log(interaction.member.joinedTimestamp);
-        // console.log(interaction.member.joinedTimestamp.toString().substring(0,10));
         console.log(target.createdAt);
         console.log(target.createdTimestamp);
 
@@ -47,28 +26,20 @@ module.exports = {
             .setColor(0x8B41C8)
             .setThumbnail(target.displayAvatarURL())
             .addFields(
-                { name: '**__Global username__**', value: `${target.globalName}`, inline: true },
+                { name: '**__Global Username__**', value: `${target.globalName}`, inline: true },
                 { name: '**__Username__**', value: `${target.username}`, inline: true },
                 { name: '**__Display Name__**', value: `${target.displayName}`, inline: true },
                 { name: '**__Is Bot__**', value: `${target.bot}`, inline: true },
                 { name: '**__Is System__**', value: `${target.system}`, inline: true },
                 { name: '**__User ID__**', value: `${target.id}`, inline: true },
-                // { name: '\u200B', value: '\u200B' }, // spacer
-
-                
-
                 { name: '**__Discord Join Date__**', value: `<t:${target.createdTimestamp.toString().substring(0,10)}:F> <t:${target.createdTimestamp.toString().substring(0,10)}:R>`},
                 { name: '**__Server Join Date__**', value: `<t:${interaction.member.joinedTimestamp.toString().substring(0,10)}:F> <t:${interaction.member.joinedTimestamp.toString().substring(0,10)}:R>`},
-
                 // { name: '\u200B', value: '\u200B' }, // spacer
-
-                
             )
             // .setImage()
             .setTimestamp()
             .setFooter({ text: client.user.tag, iconURL: client.user.displayAvatarURL(), url: client.user.displayAvatarURL() });
 
-        
         if (silence) {
             const message = await interaction.reply({
                 embeds: [userEmbed],
