6bd462865b3848256db71cfd2320e8243d4e9fa2
Joe <redsoxno10@gmail.com>
2024-03-15 00:32:07 -0700

    ada

--- src/commands/tools/user.js
+++ src/commands/tools/user.js
@@ -65,7 +65,7 @@ module.exports = {
                     userRoles += `${role}, `;
                 }
             });
-            console.log(`:${userRoles}:${userRoles.length}:`);
+            console.log(`:${userRoles}:${userRoles.toString().length}:`);
             if (userRoles.length >= 2) {
                 userRoles = userRoles.slice(0, -2);
             }
