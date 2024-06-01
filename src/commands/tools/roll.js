af567c15d633425204a631e304031ab919313f7a
Joe <redsoxno10@gmail.com>
2024-06-01 11:53:59 -0700

    init roll

---
+++ src/commands/tools/roll.js
@@ -0,0 +1,60 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('roll')
+    .setDescription("Roll dice")
+    .addStringOption(option =>
+        option.setName('sides')
+        .setDescription('How many sides. Default 20.')
+        )
+    .addStringOption(option =>
+        option.setName('options')
+        .setDescription('option1,option2...,optionX')
+        )
+    .addStringOption(option =>
+        option.setName('silence')
+        .setDescription('option1,option2...,optionX')
+        )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+    .setDMPermission(false),
+    async execute(interaction, client) {
+        const rollSize = interaction.options.getString('roll');
+        const options = interaction.options.getString('options');
+        const silence = interaction.options.getString('silence');
+
+        try {
+            if (rollSize) {
+                rollSize = rollSize.value * Math.random();
+
+                // https://discordjs.guide/popular-topics/embeds.html#embed-preview
+                const userEmbed = new EmbedBuilder()
+                    .setColor(0x8B41C8)
+                    .setAuthor({name: `${interaction.username}`, iconURL: `${interaction.user.displayAvatarURL()}`})
+                    .setThumbnail("https://nc.sudomeapizza.com/apps/files_sharing/publicpreview/QaR94zarezADXkq?file=/&fileId=4248&x=1920&y=1080&a=true&etag=8a3330768469871f1083fe65a40ed338")
+                    .addFields(
+                        { name: '**Roll:**', value: `Invalid roll value`}
+                    )
+                    .setTimestamp()
+                    .setFooter({ text: client.user.tag, iconURL: client.user.displayAvatarURL(), url: client.user.displayAvatarURL() });
+
+                const message = await interaction.reply({
+                    embeds: [userEmbed],
+                    ephemeral: true
+                });
+            }
+        } catch (error) {
+            await interaction.send({
+                embeds: new EmbedBuilder()
+                .setColor(0x8B41C8)
+                .setAuthor({name: `${interaction.username}`, iconURL: `${interaction.user.displayAvatarURL()}`})
+                .setThumbnail("https://nc.sudomeapizza.com/apps/files_sharing/publicpreview/QaR94zarezADXkq?file=/&fileId=4248&x=1920&y=1080&a=true&etag=8a3330768469871f1083fe65a40ed338")
+                .addFields(
+                    { name: '**Roll:**', value: `${rollSize} test`}
+                )
+                .setTimestamp()
+                .setFooter({ text: client.user.tag, iconURL: client.user.displayAvatarURL(), url: client.user.displayAvatarURL() })
+            });
+        }
+    }
+}
